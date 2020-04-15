65c44c4e13e62d026ce58ecb898f2a4f2f7a3606
CaroleBonacci <caroleb216@gmail.com>
2020-04-15 10:12:01 +0200

    précision que le site est en cours d'intégration

--- templates/pages/accueil.php
+++ templates/pages/accueil.php
@@ -3,12 +3,13 @@ $title = "Page d'accueil";
 require "templates/fragments/header.php";
 ?>
 
-<div class="starter-template lead">
+<div class="starter-template">
 
   <h1>Bonjour et bienvenue</h1>
-  <p class="lead">
-  Je me présente : Carole Bonacci, développeuse web beginner </p>
-  <p>C'est toujours plus agréable de faire connaissance autour d'une tasse de thé mais en attendant, voici mon site.</p>
+  <div class="lead">
+    <p>C'est toujours plus agréable de faire connaissance autour d'une tasse de thé mais en attendant, voici mon site. Je me présente : Carole Bonacci, développeuse web beginner.</p>
+  </div>
+  <p>Ce site est en cours d'intégration, merci pour votre compréhension</p>
 </div>
 
 
@@ -18,10 +19,10 @@ require "templates/fragments/header.php";
       <h2>Qui recherchez vous ?</h2>
       <div class="row">
         <div class="col-lg-5 col-md-5 col-sm-12">
-          <p>Vous souhaitez former une développeuse web à la culture de votre entreprise par le biais de l'alternance ? Vous recherchez un profil motivé, curieux et dynamique ? Rencontrons nous !</p>
+          <p>Vous souhaitez former une développeuse à la culture de votre entreprise par le biais de l'alternance ? Vous recherchez un profil motivé, curieux et dynamique ? Rencontrons nous !</p>
         </div>
         <div class="col-lg-5 col-md-5 col-sm-12">
-          <p>En challenge professionnel depuis un an et demi, j'ai découvert le développement Web que je trouve vraiment stimulant. Je n'excelle pas encore, ni en front ni en back, mais je suis passionnément curieuse. Mon objectif devenir une développeuse full stack. Pour cela, je recherche toute collaboration avec une équipe de super-développeurs qui me permettra d'apprendre et de relever différents challenges dans la joie et la bonne humeur.</p>
+          <p>En challenge professionnel depuis un an et demi, j'ai découvert le développement que je trouve vraiment stimulant. Je n'excelle pas encore, ni en front ni en back, mais je suis passionnément curieuse. Mon objectif est de devenir développeuse full stack. Pour cela, je recherche l'entreprise partenaire avec une équipe de super-développeurs qui me permettra d'apprendre et de relever différents challenges dans la joie et la bonne humeur.</p>
         </div>
         <div class="face col-lg-2 col-md-12 col-sm-12" id="face">
         </div>
@@ -32,15 +33,15 @@ require "templates/fragments/header.php";
       <h2>Who do you want to hire ?</h2>
       <div class="row ">
         <div class="col-lg-6 col-md-6 col-sm-12">
-          <p>Would you like to train a web developer in your company's culture through alternating training ? Looking for a motivated profile, full of curiosity and dynamism? Let's meet up !</p>
+          <p>Would you like to train a developer in your company's culture through alternating training ? Looking for a motivated profile, full of curiosity and dynamism? Let's meet up !</p>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-12">
-          <p>In professional reconversion for a year and a half, I discovered Web development and I find it really great. I do not excel yet neither in front nor back but I am passionately curious. I would love to become an expert developer full stack. It's my aim. For this, I seek any collaboration with a team of super-developers who would allow me to learn and realize different project, differents chalenges in joy and happiness.</p>
+          <p>In professional reconversion for a year and a half, I discovered Web development and I find it really great. I do not excel yet neither in front nor back but I am passionately curious. I would love to become an expert full stack developer. It's my aim. For this, I seek any collaboration with a team of super-developers who would allow me to learn and realize different project, differents chalenges in joy and happiness.</p>
           </div>
         </div>
       </div>
     </div>
   </div>
 </div>
-  <?php require "templates/fragments/footer.php";?>
+<?php require "templates/fragments/footer.php";?>
 
